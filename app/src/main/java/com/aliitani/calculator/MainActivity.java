86d6a7d311e14570bea434131ab2951cd3b876c6
aliitani <aliitani0@gmail.com>
2016-07-28 11:12:11 -0400

    Removed Title onCreate

--- app/src/main/java/com/aliitani/calculator/MainActivity.java
+++ app/src/main/java/com/aliitani/calculator/MainActivity.java
@@ -1,12 +1,10 @@
 package com.aliitani.calculator;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 
